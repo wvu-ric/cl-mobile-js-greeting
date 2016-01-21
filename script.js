1384015ae97dbf83781dd0091043a128d5146ed7
acurtis28 <Curtain@Austins-MacBook-Pro.local>
2016-01-21 16:21:21 -0500

    space in-between first and last

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var firstName = prompt( "What is your first name")
 var secondName = prompt("What is your second name")
 
-alert( "Hello "+ firstName + secondName)
+alert( "Hello "+ firstName + " " + secondName)
